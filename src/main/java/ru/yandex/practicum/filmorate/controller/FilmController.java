77acecc7bb4c8e34619ef197c66af2aea8342240
Dmitriy <dshestaev@yandex.ru>
2024-03-15 13:44:09 +0300

    Подготовка к отправке на ревью ветки controllers=films-users

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,65 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+
+    private Map<Integer, Film> films = new HashMap<>();
+    private int idGenerator = 1;
+    private static final LocalDate DATE_RELEASE_FIRST_FILM = LocalDate.of(1895, 12, 28);
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) throws ValidationException {
+        log.info("Добавляется новый фильм с id = {}", idGenerator);
+        validateFilm(film);
+        film.setId(idGenerator++);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film film) throws ValidationException {
+        log.info("Обновляются данные о фильме с id = {}", film.getId());
+        validateFilm(film);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @GetMapping
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    @SuppressWarnings("checkstyle:SeparatorWrap")
+    public void validateFilm(Film film) throws ValidationException {
+        if (film.getName().isEmpty()) {
+            log.error("Валидация не пройдена. Название фильма фильма пустое.");
+            throw new ValidationException("Название фильма не может быть пустым");
+        }
+        if (film.getDescription().length() > 200) {
+            log.error("Валидация не пройдена. Длина описания фильма {} больше 200 символов", film.getName());
+            throw new ValidationException("Описание не может содержать более 200 символов");
+        }
+        if (film.getReleaseDate().isBefore(DATE_RELEASE_FIRST_FILM)) {
+            log.error("Валидация не пройдена. Дата релиза фильма {} раньше изобретения кино",
+                    film.getReleaseDate().toString());
+            throw new ValidationException("Дата релиза не может быть раньше изобретения кино");
+        }
+        if (film.getDuration().getSeconds() < 0) {
+            log.error("Валидация не пройдена. Продолжительность фильма отрицательная = {}", film.getDuration().toString());
+            throw new ValidationException("Продолжительность фильма не может быть отрицательной");
+        }
+    }
 }
---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -0,0 +1,70 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UserControllerTest {
+    User testuser;
+    UserController userController;
+    Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    @BeforeEach
+    void init() {
+        testuser = User.builder()
+                .name("Тестовый пользователь")
+                .login("LoginTest")
+                .email("yandex@ya.ru")
+                .birthday(LocalDate.of(1994, 12, 06))
+                .build();
+        userController = new UserController();
+    }
+
+    @Test
+    void createNewOkUser() throws ValidationException {
+        userController.createUser(testuser);
+        Set<ConstraintViolation<User>> violations = validator.validate(testuser);
+        assertTrue(violations.size() == 0);
+    }
+
+    @Test
+    void createNewNullNameUser() throws ValidationException {
+        testuser.setName("");
+        userController.createUser(testuser);
+        Assertions.assertEquals("LoginTest", testuser.getName());
+    }
+
+    @Test
+    void createNewEmptyLoginUser() throws ValidationException {
+        testuser.setLogin("");
+        userController.createUser(testuser);
+        Set<ConstraintViolation<User>> violations = validator.validate(testuser);
+        assertTrue(violations.size() == 1);
+    }
+
+    @Test
+    void createNewBadEmailUser() throws ValidationException {
+        testuser.setEmail("yandex.ya.ru");
+        userController.createUser(testuser);
+        Set<ConstraintViolation<User>> violations = validator.validate(testuser);
+        assertTrue(violations.size() == 1);
+    }
+
+    @Test
+    void createNewBadBirthdayUser() throws ValidationException {
+        testuser.setBirthday(LocalDate.of(2025, 1, 1));
+        userController.createUser(testuser);
+        Set<ConstraintViolation<User>> violations = validator.validate(testuser);
+        assertTrue(violations.size() == 1);
+    }
+}
