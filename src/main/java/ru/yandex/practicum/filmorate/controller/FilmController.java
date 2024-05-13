fd990aa47ba6ee49b91f81f1d227a04bc0bd08e1
xakepon <145066889+xakepon@users.noreply.github.com>
2024-05-13 22:43:36 +0300

    Update FilmController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -98,7 +98,6 @@ public class FilmController {
     ) {
         return filmService.getDirectorSorted(directorId, sortBy);
     }
-    
     // Метод для вывода общих по лайкам фильмов с другим пользователем
     @GetMapping("/common")
     public List<Film> getCommonFilms(@RequestParam Integer userId,
