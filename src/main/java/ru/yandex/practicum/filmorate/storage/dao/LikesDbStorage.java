094ecced207e276a89535b515c2c3a7117dec530
Dmitriy <dshestaev@yandex.ru>
2024-05-15 18:22:55 +0300

    Устранение замечаний после тестов

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/LikesDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/LikesDbStorage.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.filmorate.storage.dao;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.storage.LikesStorage;
 
 @Slf4j
@@ -16,9 +18,13 @@ public class LikesDbStorage implements LikesStorage {
     @Override
     public void addLike(Integer userId, Integer filmId) {
         log.info("Пользователь с id = {} ставит лайк фильму с id = {}", userId, filmId);
-        String sqlQuery = "INSERT INTO likes(user_id, film_id) VALUES (?,?)";
-        jdbcTemplate.update(sqlQuery, userId, filmId);
-        log.info("Лайк поставлен");
+            String sqlQuery = "INSERT INTO likes(user_id, film_id) VALUES (?,?)";
+        try {
+            jdbcTemplate.update(sqlQuery, userId, filmId);
+            log.info("Лайк поставлен");
+        } catch (DataIntegrityViolationException exception) {
+            throw new DataIntegrityViolationException("Лайк от данного пользователя уже поставлен этому фильму!");
+        }
     }
 
     @Override
